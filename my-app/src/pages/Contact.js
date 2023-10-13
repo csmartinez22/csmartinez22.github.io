ba567b652dd2dd8c08523fba8275f43d806f7a42
csmartinez22 <joseph10655@gmail.com>
2023-10-13 16:21:18 -0400

    added contact page

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -1,5 +1,38 @@
+import "./Contact.css";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
 const Contact = () => {
-  return <h1>Contact Me</h1>;
+  return (
+    <div className="contact bg-dark">
+      <div className="title text">
+        <h1>Contact Info</h1>
+      </div>
+      <div className="body text">
+        <div className="email">
+          <div className="header">Please feel free to reach me at: </div>
+          <div>
+            Email:{" "}
+            <a href="mailto: csmartinez22@outlook.com">
+              csmartinez22@outlook.com
+            </a>
+          </div>
+          <div>Phone Number: 617-961-2158</div>
+        </div>
+        <div className="header">
+          Or follow me here!
+          <div className="icons">
+            <a href="https://www.linkedin.com/in/joseph-martinez-0b8624124">
+              <AiFillLinkedin className="icon" size={32} />
+            </a>
+            <a href="https://github.com/csmartinez22">
+              <AiFillGithub className="icon" size={32} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
