0908c93fa638e5dd3715644abc20cf99f2343810
csmartinez22 <joseph10655@gmail.com>
2023-12-04 19:10:32 -0500

    fix typo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -20,9 +20,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
